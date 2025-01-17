3ac80f3546b6a4dc0ec0d181f8293f6eb02f5699
K G B <kargb@ciencias.unam.mx>
2025-01-17 16:45:11 -0600

    test de correlacion_1

---
+++ Estadística con Python/5.correlacion(Prueba_Pearson)/1.test_correlacion.py
@@ -0,0 +1,7 @@
+import matplotlib
+import statistics as sta
+import pandas as pd
+from scipy.stats import pearsonr
+from scipy.stats import chisquare
+from scipy.stats import chi2_contingency
+import matplotlib.pyplot as plt
